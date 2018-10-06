055ce7813bc020c9b927c26342a138287cbc5622
Dmitry Dokusov <dmitry.dokusov@firstlinesoftware.com>
2018-10-06 10:42:06 +0300

    React-navigation

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@
 import React from 'react'
 import { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,34 +19,22 @@ const instructions = Platform.select({
         'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Добро пожаловать to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+const RootStack = createStackNavigator({
+    Home: HomeScreen
 });
+
+export default class App extends Component {
+    render() {
+        return <RootStack />;
+    }
+}
